061e484a64c7d882c7c59f31e4c4958d120c9f4f
J-Viral <30624865+J-Viral@users.noreply.github.com>
2025-03-01 13:51:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
+// const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 // console.log("on line 12 in index.js")
 // Routes
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 app.use('/task', taskRoutes);
 
 
